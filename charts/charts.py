db392d9083f5bbbb5bd7b43c2de957e77c022df3
Crowkas <nicolas019724@gmail.com>
2023-05-12 16:13:06 -0400

    Create random charts

---
+++ charts/charts.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import random
+
+values = 3
+values_list = []
+for i in range(values):
+    while True:
+        i = random.randint(1, 100)
+        if i not in values_list:
+            values_list.append(i)
+            break
+
+var_1, var_2, var_3 = values_list
+
+def generate_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [var_1, var_2, var_3]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels = labels)
+    plt.savefig('pie.png')
+    plt.close()
