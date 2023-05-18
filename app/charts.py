5f6c4d796cd80dbd1b3ac59037d38c38145377ae
Crowkas <nicolas019724@gmail.com>
2023-05-18 12:56:15 -0400

    Finish curse, all update

--- app/charts.py
+++ app/charts.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name,labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig(f'./bar_imgs/{name}.png')
+  plt.savefig(f'./chart_bar_imgs/{name}.png')
   plt.close()
 
 def generate_pie_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels = labels, startangle=90, labeldistance=1.1, autopct='%1.1f%%', pctdistance=0.9)
   ax.axis('equal')
-  plt.savefig(f'./pie_imgs/{name}.png')
+  plt.savefig(f'./chart_pie_imgs/{name}.png')
   plt.close()
 
 if __name__ == '__main__':
